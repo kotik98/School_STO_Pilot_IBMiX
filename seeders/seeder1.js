7f49dc88e47c91aaec971e16e363b1efa659c0fa
Igordg777 <igordg@mzil.ru>
2020-08-24 11:30:53 +0300

    - seeder.js

--- seeders/seeder1.js
+++ seeders/seeder1.js
@@ -1,122 +1,122 @@
-const config = require('config')
-const bcrypt = require('bcryptjs')
-const mongoose = require('mongoose');
-const faker = require('faker');
+// const config = require('config')
+// const bcrypt = require('bcryptjs')
+// const mongoose = require('mongoose');
+// const faker = require('faker');
 
 
-// Способ 2
+// // Способ 2
 
-/*
-mongoose.connect(
-  config.get('mongoUri'),
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  },
-);
+// /*
+// mongoose.connect(
+//   config.get('mongoUri'),
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//   },
+// );
 
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
-const user = Schema({
-  first_name: { type: String },
-  last_name: { type: String },
-  email: { type: String },
-  phone: { type: String },
-  photo: { type: String },
-  username: { type: String },
-  password: { type: String },
+// const user = Schema({
+//   first_name: { type: String },
+//   last_name: { type: String },
+//   email: { type: String },
+//   phone: { type: String },
+//   photo: { type: String },
+//   username: { type: String },
+//   password: { type: String },
 
-});
+// });
 
-const User = mongoose.model("User", user)
+// const User = mongoose.model("User", user)
 
-seedUsers = async () => {
-  const saltRounds = 10;
-  for (let i = 0; i < 10; i++) {
-    const users = new User({
-      first_name: faker.name.firstName(),
-      last_name: faker.name.lastName(),
-      email: faker.internet.email(),
-      phone: faker.phone.phoneNumberFormat(),
-      photo: faker.internet.avatar(),
-      username: faker.internet.userName(),
-      password: await bcrypt.hash('123', saltRounds)
-    });
-    users.save();
-  }
-};
+// seedUsers = async () => {
+//   const saltRounds = 10;
+//   for (let i = 0; i < 10; i++) {
+//     const users = new User({
+//       first_name: faker.name.firstName(),
+//       last_name: faker.name.lastName(),
+//       email: faker.internet.email(),
+//       phone: faker.phone.phoneNumberFormat(),
+//       photo: faker.internet.avatar(),
+//       username: faker.internet.userName(),
+//       password: await bcrypt.hash('123', saltRounds)
+//     });
+//     users.save();
+//   }
+// };
 
-seedUsers();
-*/
-//Способ 3
+// seedUsers();
+// */
+// //Способ 3
 
 
-mongoose.connect(config.get('mongoUri'), {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-});
+// mongoose.connect(config.get('mongoUri'), {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true
+// });
 
-const User = mongoose.model('User', {
-  first_name: { type: String },
-  last_name: { type: String },
-  email: { type: String },
-  phone: { type: String },
-  photo: { type: String },
-  username: { type: String },
-  password: { type: String },
-});
+// const User = mongoose.model('User', {
+//   first_name: { type: String },
+//   last_name: { type: String },
+//   email: { type: String },
+//   phone: { type: String },
+//   photo: { type: String },
+//   username: { type: String },
+//   password: { type: String },
+// });
 
-const Flights = mongoose.model('Flights', {
-  where_to: { type: String },
-  where_from: { type: String },
-  time_of_departure: { type: String },
-  time_of_arrival: { type: String },
-  flight_time: { type: String },
-  level_flights: { type: String },
-  city_photo: { type: String },
-  airport_name: { type: String },
-});
+// const Flights = mongoose.model('Flights', {
+//   where_to: { type: String },
+//   where_from: { type: String },
+//   time_of_departure: { type: String },
+//   time_of_arrival: { type: String },
+//   flight_time: { type: String },
+//   level_flights: { type: String },
+//   city_photo: { type: String },
+//   airport_name: { type: String },
+// });
 
-seedFlights = async () => {
-  for (let i = 0; i < 20; i++) {
-    const departureDate = faker.date.future().toISOString().substring(10, -10)
-    const daysToAdd = 2
-    const date = new Date(departureDate)
-    date.setDate(date.getDate() + daysToAdd)
-    const arrivalDate = date.toISOString().substring(10, -10)
+// seedFlights = async () => {
+//   for (let i = 0; i < 20; i++) {
+//     const departureDate = faker.date.future().toISOString().substring(10, -10)
+//     const daysToAdd = 2
+//     const date = new Date(departureDate)
+//     date.setDate(date.getDate() + daysToAdd)
+//     const arrivalDate = date.toISOString().substring(10, -10)
 
-    const flights = new Flights({
-      where_to: faker.address.city(),
-      where_from: faker.address.city(),
-      flight_time: faker.random.number({ min: 1, max: 48 }),
-      time_of_departure: departureDate,
-      time_of_arrival: arrivalDate,
-      level_flights: faker.random.number({ min: 1, max: 10 }),
-      city_photo: faker.image.city(),
-      airport_name: faker.name.firstName(),
-    });
-    flights.save().then(() => console.log(`+ flight ${i}`));
-  }
-}
+//     const flights = new Flights({
+//       where_to: faker.address.city(),
+//       where_from: faker.address.city(),
+//       flight_time: faker.random.number({ min: 1, max: 48 }),
+//       time_of_departure: departureDate,
+//       time_of_arrival: arrivalDate,
+//       level_flights: faker.random.number({ min: 1, max: 10 }),
+//       city_photo: faker.image.city(),
+//       airport_name: faker.name.firstName(),
+//     });
+//     flights.save().then(() => console.log(`+ flight ${i}`));
+//   }
+// }
 
-seedUsers = async () => {
-  const saltRounds = 10;
-  for (let i = 0; i < 20; i++) {
-    const user = new User({
-      first_name: faker.name.firstName(),
-      last_name: faker.name.lastName(),
-      email: faker.internet.email(),
-      phone: faker.phone.phoneNumberFormat(),
-      photo: faker.internet.avatar(),
-      username: faker.internet.userName(),
-      password: await bcrypt.hash('123', saltRounds)
-    });
-    user.save().then(() => console.log('+'));
-  }
-}
+// seedUsers = async () => {
+//   const saltRounds = 10;
+//   for (let i = 0; i < 20; i++) {
+//     const user = new User({
+//       first_name: faker.name.firstName(),
+//       last_name: faker.name.lastName(),
+//       email: faker.internet.email(),
+//       phone: faker.phone.phoneNumberFormat(),
+//       photo: faker.internet.avatar(),
+//       username: faker.internet.userName(),
+//       password: await bcrypt.hash('123', saltRounds)
+//     });
+//     user.save().then(() => console.log('+'));
+//   }
+// }
 
-// seedFlights()
-// seedUsers()
+// // seedFlights()
+// // seedUsers()
 
