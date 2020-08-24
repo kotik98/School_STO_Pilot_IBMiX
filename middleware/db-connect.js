c82259a1aa528712c3e645c82151caf8e1151b78
Igordg777 <igordg@mzil.ru>
2020-08-24 12:26:17 +0300

    + db

--- middleware/db-connect.js
+++ middleware/db-connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.spu4v.mongodb.net/IBMiX4?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aev79.azure.mongodb.net/IBMiX4?retryWrites=true&w=majority`,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
