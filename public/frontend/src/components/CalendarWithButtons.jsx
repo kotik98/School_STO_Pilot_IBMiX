3c67a1e19d9bb56ccf183d16355aff8130944674
Vladimir Romaniuk <romaniukvr@yandex.ru>
2020-09-05 18:08:20 +0300

    Added new calendar, localization and date picking

--- public/frontend/src/components/CalendarWithButtons.jsx
+++ public/frontend/src/components/CalendarWithButtons.jsx
@@ -2,10 +2,11 @@ import React, { Suspense, Component } from "react";
 import moment from 'moment';
 import {
     Card,
-    Calendar,
     Button
 } from "antd";
-
+import "react-modern-calendar-datepicker/lib/DatePicker.css";
+import { Calendar } from "react-modern-calendar-datepicker";
+import './CalendarWithButtons.css'
 class CalendarMenu extends Component {
     constructor(props) {
         super(props);
@@ -46,9 +47,114 @@ class CalendarWithButtons extends Component {
     }
 
     render() {
+      const myCustomLocale = {
+        // months list by order
+        months: [
+        'Январь',
+        'Февраль',
+        'Март',
+        'Апрель',
+        'Май',
+        'Июнь',
+        'Июль',
+        'Август',
+        'Сентябрь',
+        'Октябрь',
+        'Ноябрь',
+        'Декабрь',
+        ],
+      
+        // week days by order
+        weekDays: [
+          
+          {
+            name: 'Понедельник',
+            short: 'Пн',
+          },
+          {
+            name: 'Вторник',
+            short: 'Вт',
+          },
+          {
+            name: 'Среда',
+            short: 'Ср',
+          },
+          {
+            name: 'Четверг',
+            short: 'Чт',
+          },
+          {
+            name: 'Пятница',
+            short: 'Пт',
+          },
+          {
+            name: 'Суббота',
+            short: 'Сб',
+            isWeekend: true,
+          },
+          {
+            name: 'Воскресенье', // used for accessibility 
+            short: 'Вс', // displayed at the top of days' rows
+            isWeekend: true, // is it a formal weekend or not?
+          },
+        ],
+      
+        // just play around with this number between 0 and 6
+        weekStartingIndex: 6,
+      
+        // return a { year: number, month: number, day: number } object
+        getToday(gregorainTodayObject) {
+          return gregorainTodayObject;
+        },
+      
+        // return a native JavaScript date here
+        toNativeDate(date) {
+          return new Date(date.year, date.month - 1, date.day);
+        },
+      
+        // return a number for date's month length
+        getMonthLength(date) {
+          return new Date(date.year, date.month, 0).getDate();
+        },
+      
+        // return a transformed digit to your locale
+        transformDigit(digit) {
+          return digit;
+        },
+      
+        // texts in the date picker
+        nextMonth: 'Следующий месяц',
+        previousMonth: 'Предыдущий месяц',
+        openMonthSelector: 'Открыть выбор месяца',
+        openYearSelector: 'Открыть выбор года',
+        closeMonthSelector: 'Закрыть выбор месяца',
+        closeYearSelector: 'Закрыть выбор года',
+        defaultPlaceholder: 'Выбрать...',
+      
+        // for input range value
+        from: 'с',
+        to: 'до',
+      
+      
+        // used for input value when multi dates are selected
+        digitSeparator: ',',
+      
+        // if your provide -2 for example, year will be 2 digited
+        yearLetterSkip: 0,
+      
+        // is your language rtl or ltr?
+        // isRtl: false,
+      }
+      let days = this.props.highlighted;
+      if (days) {
+        days.forEach(day => {
+          day.className = 'workDay'
+        });
+      }
+      console.log('дни ' + days)
         return (
             <div>
-                {this.state.isMenu ? <CalendarMenu onExit={() => { this.setState({ isMenu: false }) }} /> : <Calendar defaultValue={lastDate} fullscreen={false} onSelect={() => { this.setState({ isMenu: true }); console.log('Date picked') }} />}
+                {this.state.isMenu ? <CalendarMenu onExit={() => { this.setState({ isMenu: false }) }} /> : <Calendar locale={myCustomLocale} calendarClassName='responsive-calendar' customDaysClassName={days} onChange={() => { this.setState({ isMenu: true }); console.log('Date picked') }} />}
             </div>
         )
     }
